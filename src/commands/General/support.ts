cb1e703e615f2379b2e7aa9b54f9e3c0fd17f88a
Shubha Pratim Biswas <shubhapratimbiswas@gmail.com>
2022-05-20 21:15:27 +0530

    Commit Changes

---
+++ src/commands/General/support.ts
@@ -0,0 +1,30 @@
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+  constructor(client: WAClient, handler: MessageHandler) {
+    super(client, handler, {
+      command: "support",
+      aliases: ["support"],
+      description: "Gets the support group links",
+      category: "general",
+      usage: `${client.config.prefix}Support`,
+      baseXp: 10,
+    });
+  }
+
+  run = async (M: ISimplifiedMessage): Promise<void> => {
+    await this.client.sendMessage(
+      M.sender.jid,
+      ` _*✨WELL, IF THAT WAS THE CASE THEN  ✨*_\n _*LEVI SUPPORT GROUP*_:  https://chat.whatsapp.com/EQVwc08goNlLtSivsfmpUT   
+*LEVI CASINO🎰🃏* https://chat.whatsapp.com/Jpcy4wLVdcI2eacrrEqcSD`,
+
+      MessageType.text
+    );
+
+    return void M.reply("Sent you a personal message regarding this.");
+  };
+}
---
+++ src/lib/Mongo/Models/User.ts
@@ -0,0 +1,47 @@
+import { Schema, model } from 'mongoose'
+import { IUserModel } from "../../../typings";
+const UserSchema = new Schema({
+    jid: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    ban: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    warnings: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    Xp: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    wallet: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    bank: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    lastDaily: {
+        type: Number
+    },
+    lastRob: {
+        type: Number
+    },
+    lastGamble: {
+        type: Number
+    },
+    lastSlot: {
+        type: Number
+    }
+})
+export default model<IUserModel>('users', UserSchema)
