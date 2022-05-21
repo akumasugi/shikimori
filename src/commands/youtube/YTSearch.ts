4b8be50f7ef2c55fdc8cef409d0f7f9f7ce18261
Fantox <shubhapratimbiswas@gmail.com>
2022-05-21 23:01:41 +0530

    Update YTSearch.ts

--- src/commands/youtube/YTSearch.ts
+++ src/commands/youtube/YTSearch.ts
@@ -29,14 +29,14 @@ export default class Command extends BaseCommand {
                 videos[i].author.name
             }\n 📙 *Duration:* ${videos[i].duration}\n📘 *URL:* ${videos[i].url}\n\n`
         }
-        M.reply('Downloading🎆...')
+        M.reply('Searching in YouTube...')
         this.client
             .sendMessage(M.from, text, MessageType.extendedText, {
                 quoted: M.WAMessage,
                 contextInfo: {
                     externalAdReply: {
                         title: `Search Term: ${term}`,
-                        body: `🎆BEYOND🎆`,
+                        body: `🦋 Shikimori 🦋`,
                         mediaType: 2,
                         thumbnailUrl: videos[0].thumbnail,
                         mediaUrl: videos[0].url
