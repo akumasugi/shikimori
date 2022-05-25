79214f5f2531a5d1e8019e29da4ec43a0f3339be
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-05-25 14:42:37 +0530

    Update Withdraw.ts

--- src/commands/Economy/Withdraw.ts
+++ src/commands/Economy/Withdraw.ts
@@ -48,7 +48,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `You have withdrawn *${amount} gold* from your bank.`,
-      footerText: "levi botto",
+      footerText: "shikimori",
       buttons: buttons,
       headerType: 1,
     };
